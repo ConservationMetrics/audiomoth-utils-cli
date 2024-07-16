af78b753d30d5459a42d52ecd58f3afbe3ef2c4c
Jeffrey Gerard <IamJeffG@users.noreply.github.com>
2024-07-16 11:23:35 -0500

    --max-file-duration bug is fixed
    Closes #1

--- test/command.test.js
+++ test/command.test.js
@@ -9,17 +9,24 @@ jest.mock("audiomoth-utils", () => ({
 describe("audiomoth-utils CLI", () => {
   beforeEach(() => {
     audiomothUtils.expand.mockClear();
-    // Mock console.log and console.error
     console.log = jest.fn();
     console.error = jest.fn();
-    // Mock process.exit
-    process.exit = jest.fn();
+    process.exitCode = jest.fn();
   });
 
-  test("calling with no arguments uses default values", () => {
+  test("calling with no arguments is same as calling with explicit values (the defaults)", () => {
+    // This helps find bugs with CLI argument parsing, e.g. Github issue #1
     audiomothUtils.expand.mockReturnValue({ success: true });
 
+    // No arguments - uses defaults
     command.fab(["expand-duration", "input.wav"]);
+    // Hard-coding the defaults. This tests that are CLI args get parsed correctly.
+    command.fab(["expand-duration", "input.wav", "--max-file-duration=5"]);
+
+    expect(audiomothUtils.expand).toHaveBeenCalledTimes(2);
+    expect(audiomothUtils.expand.mock.calls[0]).toEqual(
+      audiomothUtils.expand.mock.calls[1]
+    );
 
     expect(audiomothUtils.expand).toHaveBeenCalledWith(
       "input.wav",
@@ -30,14 +37,31 @@ describe("audiomoth-utils CLI", () => {
       false,
       false
     );
-
-    expect(console.log).toHaveBeenCalledWith(
-      "Expansion completed successfully."
-    );
-    expect(process.exitCode).toEqual(0);
   });
 
-  test("audiomothUtils.expand returns success", () => {
+  test("setting boolean CLI arguments (flags)", () => {
+    audiomothUtils.expand.mockReturnValue({ success: true });
+    command.fab([
+      "expand-duration",
+      "input.wav",
+      "--generate-silent-files",
+      "--align-to-second-transitions",
+    ]);
+
+    expect(audiomothUtils.expand).toHaveBeenCalledTimes(1);
+
+    expect(audiomothUtils.expand).toHaveBeenCalledWith(
+      "input.wav",
+      undefined,
+      undefined,
+      "DURATION",
+      5,
+      true,
+      true
+    );
+  });
+
+  test("when audiomothUtils.expand returns success", () => {
     audiomothUtils.expand.mockReturnValue({ success: true });
     command.fab(["expand-duration", "input.wav"]);
 
@@ -48,7 +72,7 @@ describe("audiomoth-utils CLI", () => {
     expect(process.exitCode).toEqual(0);
   });
 
-  test("audiomothUtils.expand returns failure", () => {
+  test("when audiomothUtils.expand returns failure", () => {
     audiomothUtils.expand.mockReturnValue({
       success: false,
       error: "Invalid file or some such",
@@ -63,7 +87,7 @@ describe("audiomoth-utils CLI", () => {
     expect(process.exitCode).toEqual(1);
   });
 
-  test("audiomothUtils.expand throws an exception", () => {
+  test("when audiomothUtils.expand throws an exception", () => {
     audiomothUtils.expand.mockImplementation(() => {
       throw new Error("Unexpected error");
     });
