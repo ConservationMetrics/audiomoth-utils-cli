077f5cf3eecf61448e50c0e93d166ee5c19e3f58
Jeffrey Gerard <IamJeffG@users.noreply.github.com>
2024-07-16 13:59:39 -0500

    Fix: "TypeError: Cannot read property 'then' of undefined"

--- bin/cli.js
+++ bin/cli.js
@@ -3,14 +3,10 @@
 const command = require("../lib/command");
 const program = command.makeProgram();
 
-function main() {
-  try {
-    program.parse(process.argv);
-  } catch (err) {
-    console.error("exception:", err);
-    // Recommended practice for node is set exitcode not force exit
-    process.exitCode = 8; // Exit with code 8 if an exception occurred
-  }
+try {
+  program.parse(process.argv);
+} catch (err) {
+  console.error("exception:", err);
+  // Recommended practice for node is set exitcode not force exit
+  process.exitCode = 8; // Exit with code 8 if an exception occurred
 }
-
-main().then(() => {});
